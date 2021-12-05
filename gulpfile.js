2c6317c2c14e90c44b191bcc0fb2a4eda0331306
yurez1993 <kolesnikjurij@gmail.com>
2021-12-05 20:58:56 +0200

    исправление

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
-const { src, dest, watch, parallel, series }  = require('gulp');
+const { src, dest, watch, parallel, series } = require('gulp');
 
-const scss          = require('gulp-sass');
-const concat        = require('gulp-concat');
-const browserSync   = require('browser-sync').create();
-const uglify        = require('gulp-uglify-es').default;
-const autoprefixer  = require('gulp-autoprefixer');
-const imagemin      = require('gulp-imagemin');
-const del           = require('del');
+const scss = require('gulp-sass');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const uglify = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const svgSprite = require('gulp-svg-sprite');
 
 function browsersync() {
   browserSync.init({
-    server : {
+    server: {
       baseDir: 'app/'
     }
   });
@@ -37,7 +38,19 @@ function images() {
     ))
     .pipe(dest('dist/images'))
 }
-
+function svgSprites() {
+  return src('app/images/icons/*.svg')
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: '../sprite.svg',
+          },
+        },
+      })
+    )
+    .pipe(dest('app/images'));
+}
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
@@ -57,14 +70,14 @@ function scripts() {
 
 function styles() {
   return src('app/scss/style.scss')
-      .pipe(scss({outputStyle: 'compressed'}))
-      .pipe(concat('style.min.css'))
-      .pipe(autoprefixer({
-        overrideBrowserslist: ['last 10 version'],
-        grid: true
-      }))
-      .pipe(dest('app/css'))
-      .pipe(browserSync.stream())
+    .pipe(scss({ outputStyle: 'compressed' }))
+    .pipe(concat('style.min.css'))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 10 version'],
+      grid: true
+    }))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream())
 }
 
 function build() {
@@ -73,7 +86,7 @@ function build() {
     'app/fonts/**/*',
     'app/js/main.min.js',
     'app/*.html'
-  ], {base: 'app'})
+  ], { base: 'app' })
     .pipe(dest('dist'))
 }
 
@@ -81,6 +94,7 @@ function watching() {
   watch(['app/scss/**/*.scss'], styles);
   watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
   watch(['app/*.html']).on('change', browserSync.reload);
+  watch(['app/images/icons/*.svg'], svgSprites);
 }
 
 exports.styles = styles;
@@ -88,10 +102,10 @@ exports.watching = watching;
 exports.browsersync = browsersync;
 exports.scripts = scripts;
 exports.images = images;
+exports.svgSprites = svgSprites;
 exports.cleanDist = cleanDist;
 
-
 exports.build = series(cleanDist, images, build);
-exports.default = parallel(styles ,scripts ,browsersync, watching);
+exports.default = parallel(svgSprites,styles, scripts, browsersync, watching);
 
 
